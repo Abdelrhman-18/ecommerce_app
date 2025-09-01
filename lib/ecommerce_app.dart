de8ff612e57b6dff1dad6ea668afcc7e60d5d271
Abdelrhman-18 <Abdelrhmanv61@gmail.com>
2025-09-01 06:34:33 +0300

    Initial commit

---
+++ lib/ecommerce_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'features/home_feature/screens/home_screen.dart';
+
+class EcommerceApp extends StatelessWidget {
+  const EcommerceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return HomeScreen();
+  }
+}
